f122703a3f095f792558e150de3019434dba580d
Jonsnow-willow <55383669+Jonsnow-willow@users.noreply.github.com>
2023-08-22 14:04:30 +0800

    Update select_train_set_by_symbol.py

--- tools/select_train_set_by_symbol.py
+++ tools/select_train_set_by_symbol.py
@@ -2,98 +2,12 @@
 from wizard.frames import MultiMol
 from pynep.calculate import NEP 
 from pynep.io import load_nep 
+from wizard.io import plot_e, plot_f
 import numpy as np 
 import matplotlib 
 matplotlib.use('Agg') 
 import matplotlib.pyplot as plt  
 import matplotlib.ticker as ticker 
-
-def plot_e(ed, er, symbol):
-    fig = plt.figure()
-    plt.title("NEP energy vs DFT energy", fontsize=16)
-    ed = ed - np.mean(ed)
-    er = er - np.mean(er)
-    ax = plt.gca()
-    ax.set_aspect(1)
-    xmajorLocator = ticker.MaxNLocator(5)
-    ymajorLocator = ticker.MaxNLocator(5)
-    ax.xaxis.set_major_locator(xmajorLocator)
-    ax.yaxis.set_major_locator(ymajorLocator)
-    
-    ymajorFormatter = ticker.FormatStrFormatter('%.1f') 
-    xmajorFormatter = ticker.FormatStrFormatter('%.1f') 
-    ax.xaxis.set_major_formatter(xmajorFormatter)
-    ax.yaxis.set_major_formatter(ymajorFormatter)
-    
-    ax.set_xlabel('DFT energy (eV/atom)', fontsize=14)
-    ax.set_ylabel('NEP energy (eV/atom)', fontsize=14)
-    
-    ax.spines['bottom'].set_linewidth(3)
-    ax.spines['left'].set_linewidth(3)
-    ax.spines['right'].set_linewidth(3)
-    ax.spines['top'].set_linewidth(3)    
-    ax.tick_params(labelsize=16)
-
-    
-    plt.plot([np.min(ed), np.max(ed)], [np.min(ed), np.max(ed)],
-            color='black',linewidth=3,linestyle='--',)
-    plt.scatter(ed, er, zorder=200)
-    
-    m1 = min(np.min(ed), np.min(er))
-    m2 = max(np.max(ed), np.max(er))
-    ax.set_xlim(m1, m2)
-    ax.set_ylim(m1, m2)
-
-    rmse = np.sqrt(np.mean((ed-er)**2))
-    plt.text(np.min(ed) * 0.85 + np.max(ed) * 0.15, 
-             np.min(er) * 0.15 + np.max(ed) * 0.85,
-             "RMSE: {:.3f} eV/atom".format(rmse), fontsize=14)
-    plt.savefig(f'{symbol}_e.png')
-    return fig
-
-def plot_f(fd, fr, symbol):
-    fig = plt.figure()
-    ax = plt.gca()
-    plt.title("NEP forces vs DFT forces", fontsize=16)
-    ax.set_aspect(1)
-    xmajorLocator = ticker.MaxNLocator(5)
-    ymajorLocator = ticker.MaxNLocator(5)
-    ax.xaxis.set_major_locator(xmajorLocator)
-    ax.yaxis.set_major_locator(ymajorLocator)
-    
-    ymajorFormatter = ticker.FormatStrFormatter('%.1f') 
-    xmajorFormatter = ticker.FormatStrFormatter('%.1f') 
-    ax.xaxis.set_major_formatter(xmajorFormatter)
-    ax.yaxis.set_major_formatter(ymajorFormatter)
-    
-    ax.set_xlabel('DFT forces (eV/A)', fontsize=14)
-    ax.set_ylabel('NEP forces (eV/A)', fontsize=14)
-    
-    ax.spines['bottom'].set_linewidth(2)
-    ax.spines['left'].set_linewidth(2)
-    ax.spines['right'].set_linewidth(2)
-    ax.spines['top'].set_linewidth(2)
-
-    ax.tick_params(labelsize=14)
-
-    ax.set_xlim(np.min(fd), np.max(fd))
-    ax.set_ylim(np.min(fr), np.max(fr))
-
-    plt.plot([np.min(fd), np.max(fd)], [np.min(fd), np.max(fd)],
-            color='black',linewidth=2,linestyle='--')
-    plt.scatter(fd.reshape(-1), fr.reshape(-1), s=2)
-
-    m1 = min(np.min(fd), np.min(fr))
-    m2 = max(np.max(fd), np.max(fr))
-    ax.set_xlim(m1, m2)
-    ax.set_ylim(m1, m2)
-
-    rmse = np.sqrt(np.mean((fd-fr)**2))
-    plt.text(np.min(fd) * 0.85 + np.max(fd) * 0.15, 
-             np.min(fr) * 0.15 + np.max(fr) * 0.85,
-             "RMSE: {:.3f} eV/A".format(rmse), fontsize=14)
-    plt.savefig(f'{symbol}_f.png')
-    return fig
 
 def main():
     symbols = ['Ag', 'Al', 'Au', 'Co', 'Cr', 'Cu', 'Fe', 'Mg', 'Mo', 'Ni', 'Pb', 'Pd', 'Pt', 'Ta', 'Ti', 'V', 'W', 'Zr']
