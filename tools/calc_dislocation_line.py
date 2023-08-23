54e8d8485c4102cc22cee21aa61fe54b193b14a4
Jonsnow-willow <55383669+Jonsnow-willow@users.noreply.github.com>
2023-08-23 18:33:27 +0800

    Update calc_dislocation_line.py

--- tools/calc_dislocation_line.py
+++ tools/calc_dislocation_line.py
@@ -1,27 +1,19 @@
 from ovito.io import import_file
 from ovito.modifiers import DislocationAnalysisModifier
 
-pipeline = import_file('/Volumes/Elements SE/辐照损伤工作/100keV/Cubic/0/dump.xyz')
+pipeline = import_file('dump.xyz')
 
 modifier = DislocationAnalysisModifier(trial_circuit_length = 14,circuit_stretchability = 9)
 modifier.input_crystal_structure = DislocationAnalysisModifier.Lattice.BCC
-modifier.output_type = 'all'
 pipeline.modifiers.append(modifier)
+
 data = pipeline.compute(pipeline.source.num_frames-1)
 time = data.attributes['Time'] 
 print("time: %s" % time)
-
-# Get the total length of all dislocation lines
+cell_volume = data.attributes['DislocationAnalysis.cell_volume']
+print("Cell volume: %f" % cell_volume)
 total_line_length = data.attributes['DislocationAnalysis.total_line_length']
 print("Total dislocation line length: %f" % total_line_length)
-
-# Get the length of each type of dislocation line
-dislocations = modifier.output_dislocations
-for dislocation_type in dislocations:
-    length = dislocations[dislocation_type]
-    print("Dislocation type %s length: %f" % (dislocation_type, length))
-
-# Compute the dislocation density
-cell_volume = data.attributes['DislocationAnalysis.cell_volume']
-dislocation_density = total_line_length / cell_volume
-print("Dislocation density: %f" % dislocation_density)
+print("Found %i dislocation segments" % len(data.dislocations.segments))
+for segment in data.dislocations.segments:
+    print("Segment %i: length=%f, Burgers vector=%s" % (segment.id, segment.length, segment.true_burgers_vector))
