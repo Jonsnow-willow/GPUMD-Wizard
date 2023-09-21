141b35f6791b92cebbf5e2ba491926a18fb873a1
Jonsnow-willow <55383669+Jonsnow-willow@users.noreply.github.com>
2023-09-21 18:58:36 +0800

    Update calc_interstitial_clusters.py

--- tools/calc_interstitial_clusters.py
+++ tools/calc_interstitial_clusters.py
@@ -7,36 +7,35 @@ def relax(atoms):
     mkdir_relax(atoms, run_in = ['potential ../nep.txt', 'ensemble nve', 'time_step 0',
                                      'minimize fire 1.0e-4 1000','dump_exyz 1 0 0','run 1'])
     run_gpumd('relax')
-    atoms = read_xyz('relax/dump.xyz')[-1]
+    atoms_relaxed = read_xyz('relax/dump.xyz')[-1]
     os.system('rm -rf relax')
+    return atoms_relaxed
        
-def formation_energy_sias_cluster(atoms, energy, symbol, burger, r = 5, thickness = 2, vector = (1, 0, 0)):
-    initial_num = len(atoms)
-    defects = atoms.copy() 
-    center = defects.get_center_of_mass()
-    for atom in defects:
+def formation_energy_sias_cluster(atoms, info, Rcut = 5, thickness = 2, burger = (1, 0, 0)):
+    cluster = atoms.copy() 
+    center = cluster.get_center_of_mass()
+    for atom in cluster:
         vector = atom.position - center
-        proj = abs(vector @ vector) / np.linalg.norm(vector)
+        proj = abs(vector @ burger) / np.linalg.norm(burger)
         R = np.sqrt(max(np.dot(vector, vector) - np.dot(proj, proj), 0))
-        if  R < r and proj < thickness:
-            Morph(atoms).create_self_interstitial_atom(vector=vector, index = atom.index)
-    relax(defects)
-    formation_energy = defects.info[energy] - energy * len(defects) / initial_num
-    cluster_num = len(defects) - initial_num
-    
-    dump_xyz('loop.xyz', atoms, comment=f' config_type = {symbol} {burger} {cluster_num} SIAs Cluster')
-    with open('formation_energy_loops.out', 'a') as f:
-        print(f' {symbol:<7}{burger} {cluster_num} sias Formation_Energy_Sias_Cluster: {formation_energy:.4} eV', file=f)
+        if  R < Rcut and proj < thickness:
+            Morph(cluster).create_self_interstitial_atom(burger, index = atom.index)
+    cluster_relaxed = relax(cluster)
+    formation_energy = cluster_relaxed.info['energy'] - atoms.info['energy'] * len(cluster) / len(atoms)
+    cluster_num = len(cluster) - len(atoms)
+    burger = tuple(round(x) for x in burger)
 
+    dump_xyz('SIA_cluster.xyz', atoms, comment=f' config_type = {info}{cluster_num}_SIAs_Cluster')
+    with open('SIA_cluster.out', 'a') as f:
+        print(f' {info}{cluster_num}_sias_Formation_Energy_Sias_Cluster: {formation_energy:.4} eV', file=f)
+    return formation_energy
 
 def main():
     symbol_info = SymbolInfo('W', 'bcc', 3.185)
-    atoms = symbol_info.create_bulk_atoms()
-    relax(atoms)
-    e = atoms.info['energy']
+    atoms = symbol_info.create_bulk_atoms() * (50, 50, 50)
+    atoms_relaxed = relax(atoms)
     for r in range(4,15):
-        formation_energy_sias_cluster(atoms, e, 'W', '1/2<111>', r = r, vector=(0.7,-0.7,0))
-
+        formation_energy_sias_cluster(atoms_relaxed, 'W_1/2<111>_', Rcut = r, thickness= 2.1, burger=(0.7,-0.7,0))
 
 if __name__ == "__main__":
     main()
