8d1bbb107aed0925db538bb86c8fe6f9765ad78c
Jonsnow-willow <55383669+Jonsnow-willow@users.noreply.github.com>
2023-09-22 14:24:27 +0800

    Update calc_relax.py

--- wizard_structure_calc/calc_relax.py
+++ wizard_structure_calc/calc_relax.py
@@ -1,19 +1,7 @@
 from ase.build import bulk
-from wizard.io import write_run, dump_xyz, run_gpumd
+from wizard.io import mkdir_relax, run_gpumd
 from wizard.atoms import Morph
 import os
-
-def mkdir_relax(atoms):
-    if os.path.exists('relax'):
-        raise FileExistsError('Directory "relax" already exists')
-    os.makedirs('relax')
-    original_directory = os.getcwd()
-    os.chdir('relax')
-    dump_xyz('model.xyz', atoms)
-    write_run(['potential ../nep.txt', 'velocity 300', 'time_step 1', 
-              'ensemble npt_scr 300 300 200 0 500 2000', 
-              'dump_thermo 1000', 'dump_restart 30000', 'run 30000'])
-    os.chdir(original_directory)
 
 def main():
     lc = 3.1854
