733ea55fcf0cca1244572eb6d669ab2f3e300ce2
Jonsnow-willow <55383669+Jonsnow-willow@users.noreply.github.com>
2023-10-04 00:47:37 +0800

    update chen_WRe

--- ArticleRepository/Chen_WRe/cal_properties.py
+++ ArticleRepository/Chen_WRe/cal_properties.py
@@ -11,12 +11,10 @@ def main():
     calc = LAMMPSlib(lmpcmds=cmds, log_file='log.' + symbol_info.symbol, keep_alive=True)
     atoms = symbol_info.create_bulk_atoms()
     material_calculator = MaterialCalculator(atoms, calc, symbol_info.symbol, symbol_info.structure)
-    material_calculator.isolate_atom_energy()
     material_calculator.lattice_constant()
     material_calculator.elastic_constant()
     material_calculator.eos_curve()
     material_calculator.formation_energy_vacancy()
-    material_calculator.migration_energy_vacancy()
     for nth in nths:
         material_calculator.formation_energy_divacancies(nth)
     for miller in millers:
@@ -24,7 +22,6 @@ def main():
     material_calculator.stacking_fault(a = (1,1,-1), b = (1,-1,0), miller = [1,1,2], distance = 3.185/2)
     material_calculator.stacking_fault(a = (1,1,-1), b = (1,1,2), miller = [1,1,0], distance = 3.185/2)
     material_calculator.pure_bcc_metal_screw_dipole_move()
-    material_calculator.pure_bcc_metal_screw_one_move()
     for vector in sia_vectors:
         material_calculator.formation_energy_sia(vector)
     material_calculator.formation_energy_interstitial_atom('W',[0,0,1/2],'octahedral')
