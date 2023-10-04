0f6c366ab829ef598e5286946a3a5085b374517f
Jonsnow-willow <55383669+Jonsnow-willow@users.noreply.github.com>
2023-10-04 16:19:19 +0800

    Update select_train_set_by_error.py

--- tools/select_train_set_by_error.py
+++ tools/select_train_set_by_error.py
@@ -7,9 +7,5 @@ calc = NEP('nep.txt')
 for atoms in frames:
     atoms.calc = calc
 mol = MultiMol(frames)
-
-#select = mol.select_set_by_error(0.05, 0.07, 1000)
-#dump_nep('select.xyz', select, ftype = "exyz")
-
-select = mol.select_set_by_error(0.65)
+select = mol.select_set_by_error(0.05, 0.07, 1000)
 dump_nep('select.xyz', select, ftype = "exyz")
