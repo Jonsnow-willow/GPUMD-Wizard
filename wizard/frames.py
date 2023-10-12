8f45ca9f3903954ccb7cd8871d34bb3a1913d98c
Jonsnow-willow <55383669+Jonsnow-willow@users.noreply.github.com>
2023-10-12 14:15:06 +0800

    Update frames.py

--- wizard/frames.py
+++ wizard/frames.py
@@ -6,64 +6,105 @@ class MultiMol():
     def __init__(self, frames):         
         self.frames = frames
 
-    def Devide_Train_set_by_symbol(self, symbols):
-        Train_set = []
-        for atoms in self.frames:
-            s = atoms.get_chemical_symbols()
-            if all(i in symbols for i in s):
-                Train_set.append(atoms)
-        return Train_set
-    
-    def Select_Train_set_by_symbol(self, symbols):
-        Train_set = []
-        for atoms in self.frames:
-            s = atoms.get_chemical_symbols()
-            if any(i in symbols for i in s):
-                Train_set.append(atoms)
-        return Train_set
-
-    def Devide_Train_set_by_symbol_binary(self):
-        Train_set = []
+    def select_binary(self):
+        select_set = []
+        split_set = []
         for atoms in self.frames:
             s = atoms.get_chemical_symbols()
             unique_elements = set(s)
             if len(unique_elements) < 3:
-                Train_set.append(atoms)
-        return Train_set
+                select_set.append(atoms)
+            else:
+                split_set.append(atoms)
+        return select_set, split_set
 
-    def Devide_Train_set_by_symbol_multi(self):
-        Train_set = []
+    def select_multi(self):
+        select_set = []
+        split_set = []
         for atoms in self.frames:
             s = atoms.get_chemical_symbols()
             unique_elements = set(s)
             if len(unique_elements) > 2:
-                Train_set.append(atoms)
-        return Train_set
+                select_set.append(atoms)
+            else:
+                split_set.append(atoms)
+        return select_set, split_set
     
-    def Devide_Train_set_by_symbol_num(self, num):
-        Train_set = []
+    def select_by_size(self, num):
+        select_set = []
+        split_set = []
+        for atoms in self.frames:
+            if len(atoms) == num:
+                select_set.append(atoms)
+            else:
+                split_set.append(atoms)
+        return select_set, split_set
+
+    def select_by_symbol_all(self, symbols = []):
+        select_set = []
+        split_set = []
+        for atoms in self.frames:
+            s = atoms.get_chemical_symbols()
+            if all(i in symbols for i in s):
+                select_set.append(atoms)
+            else:
+                split_set.append(atoms)
+        return select_set, split_set
+    
+    def select_by_symbol_any(self, symbols = []):
+        select_set = []
+        split_set = []
+        for atoms in self.frames:
+            s = atoms.get_chemical_symbols()
+            if any(i in symbols for i in s):
+                select_set.append(atoms)
+            else:
+                split_set.append(atoms)
+        return select_set, split_set
+    
+    def select_by_symbol_num(self, num):
+        select_set = []
+        split_set = []
         for atoms in self.frames:
             s = atoms.get_chemical_symbols()
             unique_elements = set(s)
             if len(unique_elements) == num:
-                Train_set.append(atoms)
-        return Train_set
+                select_set.append(atoms)
+            else:
+                split_set.append(atoms)
+        return select_set, split_set
     
-    def Devide_Train_set_by_num(self, num):
-        Train_set = []
-        for atoms in self.frames:
-            if len(atoms) == num:
-                Train_set.append(atoms)
-        return Train_set
-    
-    def select_set_by_error(self, error_min, error_max = 100, n = 1000000):
-        Train_set = []
+    def select_by_error(self, error_min, error_max = 100, n = 1000000):
+        select_set = []
         for atoms in self.frames:
             f_1 = np.concatenate(atoms.get_forces())
             f_2 = np.concatenate(atoms.info['forces'])
             diff = f_1 - f_2
             if np.any((diff > error_min) & (diff < error_max)):
-                Train_set.append(atoms)
-        if len(Train_set) > n:
-            Train_set = random.sample(Train_set, n)
-        return Train_set
+                select_set.append(atoms)
+        if len(select_set) > n:
+            select_set = random.sample(select_set, n)
+        return select_set
+    
+    def select_by_force(self, force_min, force_max):
+        select_set = []
+        for atoms in self.frames:
+            f = np.concatenate(atoms.info['forces'])
+            if np.all((f > force_min) & (f < force_max)):
+                select_set.append(atoms)
+        return select_set
+    
+    def subtract_isolated_atom_energy(self, isolated_atom_energy = {}):
+        for atoms in self.frames:
+            for atom in atoms:
+                atoms.info['energy'] -= isolated_atom_energy[atom.symbol]
+    
+    def cal_force_by_symbol(self, symbol):
+        dft_force = []
+        calc_force = []
+        for atoms in self.frames:
+            for atom in atoms:
+                if atom.symbol == symbol:
+                    dft_force.append(atoms.info['forces'][atom.index])
+                    calc_force.append(atoms.get_forces()[atom.index])
+        return np.array([dft_force, calc_force])
