b1966b587b2b09c9bd5fab105b6fa503fc24ff08
Jonsnow-willow <55383669+Jonsnow-willow@users.noreply.github.com>
2023-10-20 11:15:13 +0800

    Update calc_pka.py

--- wizard_structure_calc/calc_pka.py
+++ wizard_structure_calc/calc_pka.py
@@ -1,6 +1,5 @@
 from ase.build import bulk
 from wizard.io import write_run, run_gpumd, group_xyz, set_pka
-from wizard.atoms import Morph
 import numpy as np
 import os
 
@@ -28,23 +27,27 @@ def mkdir_cascade(path, pka_energy, angle, index):
               'ensemble nve','dump_exyz 1 1 1', 'run 1',
               'time_step 1 0.015', 'ensemble heat_nhc 300 200 0 0 1',
               'compute 0 500 10 temperature', 'dump_restart 10000', 
-              'dump_exyz 10000 1 1','run 300000'])
+              'dump_exyz 1000 1 1','run 70000'])
     os.chdir(original_directory)
 
 def main():
     lc = 3.1854
-    duplicate = 200
-    pka_energy = 150000
+    duplicate = 30
+    pka_energy = 5000
     angle = np.array([1, 3, 5])
     index = int(2 * (duplicate * duplicate * duplicate / 2 + duplicate * duplicate / 2 + duplicate / 2))
-    cascade_path = 'cascade'
     
-    atoms = bulk('W', 'bcc', lc, cubic = True) * (duplicate, duplicate, duplicate)
-    mkdir_relax(atoms, lc, duplicate)
-    run_gpumd('relax')
+    for i in range(10): 
+        os.makedirs(f'radiation{i}')
+        os.chdir(f'radiation{i}')
+        atoms = bulk('W', 'bcc', lc, cubic = True) * (duplicate, duplicate, duplicate)
 
-    mkdir_cascade(cascade_path, pka_energy, angle, index)
-    run_gpumd(cascade_path)
+        mkdir_relax(atoms, lc, duplicate)
+        run_gpumd('relax')
+    
+        mkdir_cascade('cascade', pka_energy, angle, index)
+        run_gpumd('cascade')
+        os.chdir('../')
 
 if __name__ == '__main__':
     main()
