c3b6a5820c5db96f47395a0825cac83513bfc4f0
Jonsnow-willow <55383669+Jonsnow-willow@users.noreply.github.com>
2023-10-22 15:34:50 +0800

    Update plot_select_structure.py

--- tools/plot_select_structure.py
+++ tools/plot_select_structure.py
@@ -9,22 +9,19 @@
 """
 from pynep.calculate import NEP
 from pynep.select import FarthestPointSample
-#from pynep.io import load_nep, dump_nep
-from wizard.io import dump_xyz, read_xyz
+from pynep.io import load_nep, dump_nep
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 
-atoms = read_xyz('example/molecular_dynamics/Langevin.xyz')
-# atoms = load_nep('train.xyz', ftype="exyz")
-calc = NEP('potential/UNEP.txt')
+
+atoms = load_nep('train.xyz', ftype="exyz")
+calc = NEP('nep.txt')
 des = np.array([np.mean(calc.get_property('descriptor', i), axis=0) for i in atoms])
 sampler = FarthestPointSample(min_distance=0.008)
 selected_i = sampler.select(des, [])
 train_set = [atoms[i] for i in selected_i]
-for a in train_set:
-    dump_xyz('selected_train.xyz', a)
-#dump_nep('selected_train.xyz', train_set, ftype="exyz")
+dump_nep('selected_train.xyz', train_set, ftype="exyz")
 
 reducer = PCA(n_components=2)
 reducer.fit(des)
