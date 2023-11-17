ac9f935eb6bc605723bf8dc55a38024e637c3c64
Jonsnow-willow <55383669+Jonsnow-willow@users.noreply.github.com>
2023-11-17 13:21:46 +0800

    Create select_by_error.py

---
+++ tools/select_by_error.py
@@ -0,0 +1,23 @@
+from pynep.io import load_nep, dump_nep
+from pynep.calculate import NEP
+from wizard.frames import MultiMol
+from wizard.io import plot_force_results
+
+frames = load_nep('train.xyz', ftype='exyz')
+calc = NEP('nep.txt')
+for atoms in frames:
+    atoms.calc = calc
+#修改力的误差范围
+select_set, split_set = MultiMol(frames).select_by_error(0.3, 0.5)
+dump_nep('select.xyz', select_set, ftype='exyz')
+dump_nep('split.xyz', split_set, ftype='exyz')
+plot_force_results(select_set, [calc], ['error_[0.3,0.5]'])
+
+#在已经筛选后的结构中，随机选择
+random_set, _ = MultiMol(select_set).select_random(100)
+dump_nep('random.xyz', random_set, ftype='exyz')
+
+
+
+
+
