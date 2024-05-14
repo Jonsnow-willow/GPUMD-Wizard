1824ab1f89f629b9fc42ef8e54b85ed79ac7d4df
Jonsnow-willow <55383669+Jonsnow-willow@users.noreply.github.com>
2024-05-14 16:09:29 +0800

    Update molecular_dynamics.py

--- wizard/molecular_dynamics.py
+++ wizard/molecular_dynamics.py
@@ -1,27 +1,34 @@
 from ase import units
-from ase.lattice.cubic import FaceCenteredCubic
 from ase.md.velocitydistribution import MaxwellBoltzmannDistribution
 from ase.md.verlet import VelocityVerlet
 from ase.md.langevin import Langevin
 from ase.md.nptberendsen import NPTBerendsen
+from ase.md.nvtberendsen import NVTBerendsen
 from ase.io.trajectory import Trajectory
 
 class MolecularDynamics:
-    def __init__(self, atoms, calc, timestep = units.fs, dump_interval = 100, report_interval = 100):
+    def __init__(self, atoms, calc, timestep = units.fs, dump_interval = 100, report_interval = 100, A_per_step = None):
         self.atoms = atoms
         self.calc = calc
         self.timestep = timestep
         self.dump_interval = dump_interval
         self.report_interval = report_interval
         self.nsteps = 0
+        self.A_per_step = A_per_step
 
     def report(self):  
         atoms = self.atoms
-        self.nsteps += self.report_interval
         epot = atoms.get_potential_energy() / len(atoms)
         ekin = atoms.get_kinetic_energy() / len(atoms)
         print('Step: %d Energy per atom: Epot = %.3feV  Ekin = %.3feV (T=%3.0fK)  '
             'Etot = %.3feV' % (self.nsteps, epot, ekin, ekin / (1.5 * units.kB), epot + ekin))
+        self.nsteps += self.report_interval
+
+    def deform(self):
+        atoms = self.atoms
+        cell = atoms.get_cell()
+        cell[2, 2] += self.A_per_step
+        atoms.set_cell(cell, scale_atoms=True)
 
     def NVE(self, temperature_K = 300, steps = 10000):
         atoms = self.atoms
@@ -40,9 +47,11 @@ class MolecularDynamics:
         traj = Trajectory('NVT.traj', 'w', atoms)
         if 'velocities' not in self.atoms.info or self.atoms.info['velocities'] is None:
             MaxwellBoltzmannDistribution(self.atoms, temperature_K=temperature_K)
-        dyn = Langevin(self.atoms, self.timestep, temperature_K, 0.02)
+        dyn = NVTBerendsen(self.atoms, timestep=self.timestep, temperature_K=temperature_K, taut=100 * units.fs)
         dyn.attach(self.report, interval=self.report_interval)
         dyn.attach(traj.write, interval=self.dump_interval)
+        if self.A_per_step is not None:
+            dyn.attach(self.deform)
         dyn.run(steps)
 
     def NPT(self, temperature_K = 300, pressure = 0, steps = 10000):
@@ -57,4 +66,3 @@ class MolecularDynamics:
         dyn.attach(self.report, interval=self.report_interval)
         dyn.attach(traj.write, interval=self.dump_interval)
         dyn.run(steps)
-
