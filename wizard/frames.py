fb8389d89f059d85e5b612912805c995d56f831d
Jonsnow-willow <55383669+Jonsnow-willow@users.noreply.github.com>
2024-08-04 22:49:42 +0800

    Update frames.py

--- wizard/frames.py
+++ wizard/frames.py
@@ -1,7 +1,8 @@
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt 
-from wizard.io import dump_xyz
+from wizard.io import dump_xyz, relax
+from wizard.atoms import Morph
 import numpy as np 
 import random 
 import re
@@ -18,15 +19,24 @@ class MultiMol():
                 select_set.append(atoms)
         return select_set
     
-    def split_by_size(self, num):
+    def select_by_symbols(self, *symbols):
         select_set = []
-        split_set = []
         for atoms in self.frames:
-            if len(atoms) == num:
-                select_set.append(atoms)
+            chemical_symbols = atoms.get_chemical_symbols()
+            if any(chemical_symbol not in symbols for chemical_symbol in chemical_symbols):
+                continue
             else:
-                split_set.append(atoms)
-        return select_set, split_set
+                select_set.append(atoms)
+        return select_set
+    
+    def select_by_num_of_symbols(self, num):
+        select_set = []
+        for atoms in self.frames:
+            s = atoms.get_chemical_symbols()
+            unique_elements = set(s)
+            if len(unique_elements) == num:
+                select_set.append(atoms)
+        return select_set
 
     def select_by_formulars(self, *formulars):
         select_set = []
@@ -41,66 +51,12 @@ class MultiMol():
                     break
         return select_set
     
-    def select_by_symbols(self, *symbols):
-        select_set = []
-        for atoms in self.frames:
-            chemical_symbols = atoms.get_chemical_symbols()
-            if any(chemical_symbol not in symbols for chemical_symbol in chemical_symbols):
-                continue
-            else:
-                select_set.append(atoms)
-        return select_set
-    
     def select_by_config_type(self, config_type):
         select_set = []
         for atoms in self.frames:
             if atoms.info['config_type'] == config_type:
                 select_set.append(atoms)
         return select_set
-    
-    def select_by_num_of_symbols(self, num):
-        select_set = []
-        for atoms in self.frames:
-            s = atoms.get_chemical_symbols()
-            unique_elements = set(s)
-            if len(unique_elements) == num:
-                select_set.append(atoms)
-        return select_set
-    
-    def split_by_num_of_symbols(self, num):
-        select_set = []
-        split_set = []
-        for atoms in self.frames:
-            s = atoms.get_chemical_symbols()
-            unique_elements = set(s)
-            if len(unique_elements) == num:
-                select_set.append(atoms)
-            else:
-                split_set.append(atoms)
-        return select_set, split_set
-
-    def select_by_force_error(self, error_min, error_max = 100):
-        select_set = []
-        for atoms in self.frames:
-            f_1 = np.concatenate(atoms.get_forces())
-            f_2 = np.concatenate(atoms.info['forces'])
-            diff = abs(f_1 - f_2)
-            if np.any((diff > error_min) & (diff < error_max)):
-                select_set.append(atoms)
-        return select_set
-    
-    def split_by_force_error(self, error_min, error_max = 100):
-        select_set = []
-        split_set = []
-        for atoms in self.frames:
-            f_1 = np.concatenate(atoms.get_forces())
-            f_2 = np.concatenate(atoms.info['forces'])
-            diff = abs(f_1 - f_2)
-            if np.any((diff > error_min) & (diff < error_max)):
-                select_set.append(atoms)
-            else:
-                split_set.append(atoms)
-        return select_set, split_set
     
     def select_by_force(self, force_min, force_max):
         select_set = []
@@ -110,47 +66,60 @@ class MultiMol():
                 select_set.append(atoms)
         return select_set
     
-    def split_by_force(self, force_min, force_max):
-        select_set = []
-        split_set = []
-        for atoms in self.frames:
-            f = np.concatenate(atoms.info['forces'])
-            if np.all((f > force_min) & (f < force_max)):
-                select_set.append(atoms)
-            else:
-                split_set.append(atoms)
-        return select_set, split_set
-
     def select_random(self, num):
         select_set = []
         select_set = random.sample(self.frames, num)
         return select_set
     
-    def split_random(self, num):
+    def select_by_force_error(self, calc, error_min = 0, error_max = 100):
         select_set = []
-        split_set = []
-        select_set = random.sample(self.frames, num)
-        split_set = [i for i in self.frames if i not in select_set]
-        return select_set, split_set
-    
-    def subtract_frames(self, frames):
-        select_set = [i for i in self.frames if i not in frames]
+        for atoms in self.frames:
+            atoms.calc = calc
+            f_1 = np.concatenate(atoms.get_forces())
+            f_2 = np.concatenate(atoms.info['forces'])
+            diff = abs(f_1 - f_2)
+            if np.any((diff > error_min) & (diff < error_max)):
+                select_set.append(atoms)
         return select_set
     
-    def subtract_isolated_atom_energy(self, isolated_atom_energy = {}):
+    def select_by_coherent_energy(self, calc, coh = {}, e_min = 0, emax = 10000):
+        select_set = []
         for atoms in self.frames:
+            atoms.calc = calc
+            energy = atoms.get_potential_energy()
             for atom in atoms:
-                atoms.info['energy'] -= isolated_atom_energy[atom.symbol]
-    
-    def cal_force_by_symbol(self, symbol):
-        dft_force = []
-        calc_force = []
+                energy -= coh[atom.symbol]
+            if energy > e_min and energy < emax:
+                select_set.append(atoms)
+        return select_set
+       
+    def active_learning(self, calcs, error_min = 0, error_max = 100, n = 1000000):
+        Train_set = []
         for atoms in self.frames:
+            forces = []
+            for calc in calcs:
+                atoms.calc = calc
+                forces.append(atoms.get_forces())
+            forces = np.std(forces, axis=0)
+            forces = np.linalg.norm(forces, axis=1)
+            delta = np.max(forces)
+            if delta > error_min and delta < error_max:
+                Train_set.append(atoms)
+                
+        if len(Train_set) > n:
+            Train_set = random.sample(Train_set, n)
+        return Train_set
+
+    def calculate_forces_for_symbol(self, calc, symbol):
+        ref_force = []
+        model_force = []
+        for atoms in self.frames:
+            atoms.calc = calc
             for atom in atoms:
                 if atom.symbol == symbol:
-                    dft_force.append(atoms.info['forces'][atom.index])
-                    calc_force.append(atoms.get_forces()[atom.index])
-        return np.array([dft_force, calc_force])
+                    ref_force.append(atoms.info['forces'][atom.index])
+                    model_force.append(atoms.get_forces()[atom.index])
+        return np.array([ref_force, model_force])
 
     def deform(self, scale = np.arange(0.95, 1.06, 0.05)):
         frames = []
@@ -179,6 +148,23 @@ class MultiMol():
             frames.append(atoms_copy)
         return frames
     
+    def get_vacancies(self, calc = None, num = [1,3,6]):
+        frames = []
+        for atoms in self.frames:
+            for n in num:
+                atoms_copy = atoms.copy()
+                Morph(atoms_copy).create_vacancies(n)
+                if calc is not None:
+                    atoms_copy.calc = calc
+                    relax(atoms_copy, steps=50)
+                frames.append(atoms_copy)
+        return frames
+    
+    def subtract_isolated_atom_energy(self, isolated_atom_energy = {}):
+        for atoms in self.frames:
+            for atom in atoms:
+                atoms.info['energy'] -= isolated_atom_energy[atom.symbol]
+
     def shuffle_symbols(self):
         for atoms in self.frames:
             s = atoms.get_chemical_symbols()
@@ -189,6 +175,10 @@ class MultiMol():
         for atoms in self.frames:
             dump_xyz(filename, atoms)
 
+    def subtract_frames(self, frames):
+        select_set = [i for i in self.frames if i not in frames]
+        return select_set
+    
     def plot_force_results(self, calcs, labels = None, e_val = [None, None], f_val = [None, None]):
         plt.rcParams["figure.figsize"] = (12, 6)
         plt.rcParams.update({"font.size": 10, "text.usetex": False})
