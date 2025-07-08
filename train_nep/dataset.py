38adbbd6e0dfd9f76cd69805604e21cac8102c3e
Jiahui Liu <602585882@qq.com>
2025-07-08 14:31:38 +0800

    Update dataset.py

--- train_nep/dataset.py
+++ train_nep/dataset.py
@@ -33,56 +33,84 @@ def pad_distances(distances, max_nbs=None, pad_value=-1):
 
 def collate_fn(batch):
     positions = [item["positions"] for item in batch]
-    types = [item["types"] for item in batch]
-    neighbors = [item["neighbors"] for item in batch]
-    distances = [item["distances"] for item in batch]
-    
-    n_atoms = [len(pos) for pos in positions]
-    
     positions_batch = torch.cat(positions, dim=0)
+
+    types = [item["types"] for item in batch]
     types_batch = torch.cat(types, dim=0)
-    
-    neighbors_batch = []
-    distances_batch = []
+
+    n_atoms = [len(pos) for pos in positions]
+    neighbors_radial = [item["neighbors_radial"] for item in batch]
+    distances_radial = [item["distances_radial"] for item in batch]
+    neighbors_angular = [item["neighbors_angular"] for item in batch]
+    distances_angular = [item["distances_angular"] for item in batch]
+
+    neighbors_radial_batch = []
+    distances_radial_batch = []
+    neighbors_angular_batch = []
+    distances_angular_batch = []
     atom_offset = 0
-    
-    for i, (nbs, dists) in enumerate(zip(neighbors, distances)):
-        valid_mask = nbs != -1
-        nbs_updated = nbs.clone()
-        nbs_updated[valid_mask] += atom_offset
-        neighbors_batch.append(nbs_updated)
-        distances_batch.append(dists)
+
+    for i in range(len(batch)):
+
+        nbs_rad = neighbors_radial[i]
+        dists_rad = distances_radial[i]
+        valid_mask_rad = nbs_rad != -1
+        nbs_rad_updated = nbs_rad.clone()
+        nbs_rad_updated[valid_mask_rad] += atom_offset
+        neighbors_radial_batch.append(nbs_rad_updated)
+        distances_radial_batch.append(dists_rad)
+
+        nbs_ang = neighbors_angular[i]
+        dists_ang = distances_angular[i]
+        valid_mask_ang = nbs_ang != -1
+        nbs_ang_updated = nbs_ang.clone()
+        nbs_ang_updated[valid_mask_ang] += atom_offset
+        neighbors_angular_batch.append(nbs_ang_updated)
+        distances_angular_batch.append(dists_ang)
+
         atom_offset += n_atoms[i]
-    
-    neighbors_batch = torch.cat(neighbors_batch, dim=0)
-    distances_batch = torch.cat(distances_batch, dim=0)
-    
+
+    neighbors_radial_batch = torch.cat(neighbors_radial_batch, dim=0)
+    distances_radial_batch = torch.cat(distances_radial_batch, dim=0)
+    neighbors_angular_batch = torch.cat(neighbors_angular_batch, dim=0)
+    distances_angular_batch = torch.cat(distances_angular_batch, dim=0)
+
     return {
         "positions": positions_batch,
         "types": types_batch,
-        "neighbors": neighbors_batch,
-        "distances": distances_batch,
+        "neighbors_radial": neighbors_radial_batch,
+        "distances_radial": distances_radial_batch,
+        "neighbors_angular": neighbors_angular_batch,
+        "distances_angular": distances_angular_batch,
         "n_atoms": torch.tensor(n_atoms),
         "batch_size": len(batch)
     }
 
 class StructureDataset(Dataset):
-    def __init__(self, filepath, cutoff):
+    def __init__(self, filepath, cutoff_radial, cutoff_angular):
         self.structures = read_xyz(filepath)
-        self.cutoff = cutoff
+        self.cutoff_radial = cutoff_radial
+        self.cutoff_angular = cutoff_angular
         self.data = [self.process(atoms) for atoms in self.structures]
     
     def process(self, atoms):
         coords = atoms.get_positions()
         types = atoms.get_atomic_numbers()
-        neighbors, distances = find_neighbor(atoms, self.cutoff)
-        neighbors_pad = pad_neighbors(neighbors)
-        distances_pad = pad_distances(distances)
+
+        neighbors_rad, distances_rad = find_neighbor(atoms, self.cutoff_radial)
+        neighbors_rad_pad = pad_neighbors(neighbors_rad)
+        distances_rad_pad = pad_distances(distances_rad)
+
+        neighbors_ang, distances_ang = find_neighbor(atoms, self.cutoff_angular)
+        neighbors_ang_pad = pad_neighbors(neighbors_ang)
+        distances_ang_pad = pad_distances(distances_ang)
         return {
             "positions": torch.tensor(coords, dtype=torch.float32),
             "types": torch.tensor(types, dtype=torch.long),
-            "neighbors": neighbors_pad,
-            "distances": distances_pad,
+            "neighbors_radial": neighbors_rad_pad,
+            "distances_radial": distances_rad_pad,
+            "neighbors_angular": neighbors_ang_pad,
+            "distances_angular": distances_ang_pad,
         }
     
     def __len__(self):
