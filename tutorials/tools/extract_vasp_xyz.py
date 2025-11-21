ac330a80b4f241b57f473ae9769daf0800a19540
Jiahui Liu <602585882@qq.com>
2025-11-21 09:22:02 +0800

    Update extract_vasp_xyz.py

--- tutorials/tools/extract_vasp_xyz.py
+++ tutorials/tools/extract_vasp_xyz.py
@@ -4,14 +4,15 @@
 # GPUMD:
 # https://github.com/brucefan1983/GPUMD
 #
-# Copyright (c) 2022 wang laosi, licensed under MIT License
+# Copyright 2017 Zheyong Fan and GPUMD development team
+# Released under the GNU General Public License v3 
+# (see project LICENSE or https://www.gnu.org/licenses/).
 # -----------------------------------------------------------------------------
 """
 
-import os, sys
+import os
 import numpy as np
 from ase.io import read, write
-from ase import Atoms, Atom
 from tqdm import tqdm
 
 
@@ -23,36 +24,39 @@ def Convert_atoms(atom):
     del atom.calc.results['stress']
     del atom.calc.results['free_energy']
 
-
 def find_vasprun(start_path='.'):
     result = []
-    for root, dirs, files in os.walk(start_path):
+    for root, _, files in os.walk(start_path):
         if 'vasprun.xml' in files:
             result.append(os.path.join(root, 'vasprun.xml'))
     return result
 
-Path = '.'
-file_list = find_vasprun(Path)
+def main():
+    Path = '.'
+    file_list = find_vasprun(Path)
 
-cnum = 0     # total number of configuration
-atoms_list, err_list = [], []
-for dir_name in tqdm(file_list):
-    try:
-        atoms = read(dir_name.strip('\n'), index=":")
-    except:
-        err_list.append(dir_name)
-        continue
-    for ai in range(len(atoms)):
-        Convert_atoms(atoms[ai])
-        atoms_list.append(atoms[ai])
-    cnum += len(atoms)
+    cnum = 0     # total number of configuration
+    atoms_list, err_list = [], []
+    for dir_name in tqdm(file_list):
+        try:
+            atoms = read(dir_name.strip('\n'), index=":")
+        except:
+            err_list.append(dir_name)
+            continue
+        for ai in range(len(atoms)):
+            Convert_atoms(atoms[ai])
+            atoms_list.append(atoms[ai])
+        cnum += len(atoms)
 
-write(Path + '/train.xyz', atoms_list, format='extxyz')
-print('The total number of configurations is: {} \n'.format(cnum))
+    write(Path + '/train.xyz', atoms_list, format='extxyz')
+    print('The total number of configurations is: {} \n'.format(cnum))
 
-if err_list:
-    print("The list of failed calculation files is as follows.")
-    for err_dirname in err_list:
-        print(err_dirname)
-else:
-    print("All calculations are successful!")
+    if err_list:
+        print("The list of failed calculation files is as follows.")
+        for err_dirname in err_list:
+            print(err_dirname)
+    else:
+        print("All calculations are successful!")
+
+if __name__ == "__main__":
+    main()
