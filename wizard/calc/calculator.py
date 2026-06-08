cfdf9bd4428b4187309ed4973d87f9ba15c1d69f
Jiahui Liu <602585882@qq.com>
2026-06-08 21:13:18 +0800

    Update calculator.py

--- wizard/calc/calculator.py
+++ wizard/calc/calculator.py
@@ -490,4 +490,172 @@ class AlloyCalculator(MaterialCalculator):
         atoms = alloy_info.create_bulk_atoms(supercell = supercell)
         super().__init__(atoms, calculator, clamped, **kwargs)
 
+    def stacking_fault(self, a, b, miller, distance):
+        '''
+        ---------------------------------------------------------------------------------------------------
+        For FCC-Al                  |   For BCC-Nb
+        surf. I        surf. II     |   surf. I              surf. II            surf. III
+        (-1, 1,  0)    (-1, 1,  0)  |   (-1, 1, -2)          ( 1,-1,  0)         ( 1, -4/5, 1/5)
+        ( 1, 1, -2)    ( 1, 1,  0)  |   (-1, 1,  1) <111>    ( 1, 1, -1) <111>   ( 1,  1,    -1)  <111>
+        ( 1, 1,  1)    ( 0, 0,  1)  |   ( 1, 1,  0) {110}    ( 1, 1,  2) {112}   ( 1,  2,     3)  {123}
+                                    
+        ---------------------------------------------------------------------------------------------------
+        For HCP-Ti 
+        for basal {0001} Normal--[0001]          for prism {10-10} Normal--[10-10]              
+        uvws =  [[-2,1,1,0],  [[-1, 0,0]          uvws =  [[-1,2,-1, 0],    [[0,1,0] 
+                 [0,-1,1,0],   [-1,-2,0]                   [0, 0, 0, 1],     [0,0,1] 
+                 [0, 0,0,1]]   [ 0, 0,1]]                  [1, 0,-1, 0]]     [2,1,0]] 
+             
+        ---------------------------------------------------------------------------------------------------                                                       
+        for Pyramidal I narrow  {10-11} Normal: None    for Pyramidal I wide {10-11} Normal: None   
+        uvws = [[-1, 2,-1, 0],  [0,  1,0]               uvws = [[-1,-1,  2, 3],     [[-1,-1,1]      
+                [-1, 0, 1, 2],  [-2,-1,2]                       [-1, 0,  1, 2],      [-2,-1,2]      
+                 {1,  0,-1, 1}]                                  {1,  0, -1, 1}]                 
+                                                                                                                                                         
+        for Pyramidal II {11-22} Normal       
+        uvws =  [[-1,-1, 2,3],     [[-1,-1,1]
+                [-1, 1, 0,0],      [-1, 1,0]
+                {1,1,-2,  2}       [N,  N,N]]                                                                         
+        ---------------------------------------------------------------------------------------------------
+        Hexagonal Miller direction indices to Miller-Bravais indices and back:
+        [-1,-1,2,3] = [-1,-1,1]
+        [-1, 1,0,0] = [-1, 1,0]
+        ---------------------------------------------------------------------------------------------------
+        ''' 
+        atoms = self.atoms.copy()
+        atoms.calc = self.calc
+
+        slab = cut(atoms, a, b, clength=None, origo=(0,0,0), nlayers = 18, extend=1.0, tolerance=0.01, maxatoms=None)
+        rotate(slab, a,(1,0,0),b,(0,1,0), center=(0,0,0))
+        slab.calc = self.calc
+        relax(slab)
+        slab.center(axis=2)
+        #slab.constraints = FixAtoms(indices=[atom.index for atom in slab if atom.position[2] < 1/2 * slab.cell[2][2]])
+        box = slab.get_cell()
+        S = (box[0][0] * box[1][1] - box[0][1] * box[1][0]) * 2
+
+        shift_distance = np.linalg.norm(np.array(a)) * distance
+        shift_indices = [atom.index for atom in slab if atom.position[2] > 1/2 * slab.cell[2][2]]
+        slide_steps = shift_distance / 10
+        
+        energies = []
+        for i in range(11):
+            slab_shift = slab.copy()
+            slab_shift.positions[shift_indices] += [slide_steps * i, 0,0]
+            slab_shift.calc = self.calc
+            relax(slab_shift, fixed_line=True)
+            defects_energy = slab_shift.get_potential_energy() / S
+            energies.append(defects_energy)
+            dump_xyz('MaterialProperties.xyz', slab_shift)
+
+        energies = [e - energies[0] for e in energies]
+        with open('MaterialProperties.out', 'a') as f:
+            print(f' {self.info:<7}{miller} Stacking_Fault: {max(energies) * 1000:.4f} meV/A^2', file=f)
+
+        plt.plot(np.linspace(0, 1, len(energies)), energies, marker='o', label=f'{self.info}')  
+        plt.legend()
+        plt.savefig(f'{self.info}_stacking_fault_{miller}.png')
+        plt.close()
+        return energies
+    
+    def bcc_metal_screw_dipole_move(self, fmax = 0.02, steps = 500):
+        lc = self.lc
+        symbols = []
+        compositions = []
+        for symbol, composition in re.findall(r'([A-Z][a-z]*)(\d*)', self.formula):
+            symbols.append(symbol)
+            compositions.append(int(composition) if composition else 1)
+        if len(symbols) > 1:
+            element_ratio = np.array(compositions) / sum(compositions)
+            element_counts = np.ceil(element_ratio * 135).astype(int)
+            symbols = np.repeat(symbols, element_counts)
+            np.random.shuffle(symbols)
+            sym = symbols[:135]
+        else:
+            sym = [symbols[0] for _ in range(135)]
+
+        initial_screw = read_xyz(str(files('nexus.calc').joinpath('str', 'Fe_screw.xyz')))
+        for i in initial_screw:
+            i.set_chemical_symbols(sym)
+            i.calc = self.calc
+
+        initial = initial_screw[0]
+        final = initial_screw[1]
+
+        unit_cell = initial.cell.copy()
+        initial.set_cell(lc * unit_cell, scale_atoms=True)
+        relax(initial, method='ucf')
+        initial_cell = initial.cell.copy()
+        final.set_cell(initial_cell, scale_atoms=True)
+        relax(final, method='ucf')
+
+        images = [initial] + [initial.copy() for i in range(15)] + [final]
+        for i in images:
+            i.calc = self.calc
+        neb = NEB(images, allow_shared_calculator=True)
+        neb.interpolate()    
+        optimizer = FIRE(neb)
+        optimizer.run(fmax=fmax, steps=steps)
+        energies = [image.get_potential_energy() / 2  for image in images]
+        energies = np.array(energies)
+        energies -= min(energies)
+        for image in images:
+            dump_xyz('MaterialProperties.xyz', image)  
+
+        plt.plot(np.linspace(0, 1, len(energies)), energies, marker='o', label=f'{self.formula}')
+        plt.legend()
+        plt.savefig(f'{self.formula}_screw_dipole_move.png')
+        plt.close()
+        return energies
+
+    def bcc_metal_screw_one_move(self, fmax = 0.02, steps = 500):
+        lc = self.lc
+        symbols = []
+        compositions = []
+        for symbol, composition in re.findall(r'([A-Z][a-z]*)(\d*)', self.formula):
+            symbols.append(symbol)
+            compositions.append(int(composition) if composition else 1)
+        if len(symbols) > 1:
+            element_ratio = np.array(compositions) / sum(compositions)
+            element_counts = np.ceil(element_ratio * 135).astype(int)
+            symbols = np.repeat(symbols, element_counts)
+            np.random.shuffle(symbols)
+            sym = symbols[:135]
+        else:
+            sym = [symbols[0] for _ in range(135)]
+        
+        initial_screw = read_xyz(str(files('nexus.calc').joinpath('str', 'Fe_screw.xyz')))
+        for i in initial_screw:
+            i.set_chemical_symbols(sym)
+            i.calc = self.calc
+
+        initial = initial_screw[0]
+        final = initial_screw[2]
+
+        unit_cell = initial.cell.copy()
+        initial.set_cell(lc * unit_cell, scale_atoms=True)
+        relax(initial, method='ucf')
+        initial_cell = initial.cell.copy()
+        final.set_cell(initial_cell, scale_atoms=True)
+        relax(final, method='ucf')
+
+        images = [initial] + [initial.copy() for i in range(15)] + [final]
+        for i in images:
+            i.calc = self.calc
+        neb = NEB(images, allow_shared_calculator=True)
+        neb.interpolate()    
+        optimizer = FIRE(neb)
+        optimizer.run(fmax=fmax, steps=steps)
+        energies = [image.get_potential_energy() for image in images]
+        energies = np.array(energies)
+        energies -= min(energies)
+        for image in images:
+            dump_xyz('MaterialProperties.xyz', image)  
+
+        plt.plot(np.linspace(0, 1, len(energies)), energies, marker='o', label=f'{self.formula}')
+        plt.legend()
+        plt.savefig(f'{self.formula}_screw_one_move.png')
+        plt.close()
+        return energies
+
     
