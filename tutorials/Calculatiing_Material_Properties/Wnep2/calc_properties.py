f095936fc52c5d65c38666f78b6fb64f4559f922
Jiahui Liu <602585882@qq.com>
2026-06-09 15:10:01 +0800

    Update calc_properties.py

--- tutorials/Calculatiing_Material_Properties/Wnep2/calc_properties.py
+++ tutorials/Calculatiing_Material_Properties/Wnep2/calc_properties.py
@@ -5,27 +5,9 @@
 # ----------------------------------------------------------------------------------
 """
 
-import numpy as np
-from ase import Atom
-
 from wizard.model.atoms import AlloyInfo
 from wizard.calc.calculator import AlloyCalculator
-from wizard.core.minimize import relax
-from wizard.utils.io import dump_xyz
 from calorine.calculators import CPUNEP
-
-
-def formation_energy_interstitial_site(material_calculator, symbol, scaled_position, site_name):
-    atoms = material_calculator.atoms.copy()
-    position = np.dot(np.asarray(scaled_position, dtype=float), atoms.get_cell())
-    atoms.append(Atom(symbol=symbol, position=position))
-    atoms.calc = material_calculator.calc
-    relax(atoms, **material_calculator.kwargs)
-    formation_energy = atoms.get_potential_energy() - material_calculator.atom_energy * len(atoms)
-    dump_xyz('MaterialProperties.xyz', atoms)
-    with open('MaterialProperties.out', 'a') as f:
-        print(f' {symbol:<10}{site_name} Formation_Energy_Interstitial: {formation_energy:.4f} eV', file=f)
-    return formation_energy
 
 
 def main():
@@ -52,9 +34,8 @@ def main():
     material_calculator.bcc_metal_screw_one_move()
     for vector in sia_vectors:
         material_calculator.formation_energy_sia(vector)
-    formation_energy_interstitial_site(material_calculator, 'W', [0,0,1/2], 'octahedral')
-    formation_energy_interstitial_site(material_calculator, 'W', [1/4,0,1/2], 'tetrahedral')
+    material_calculator.formation_energy_interstitial('W', 'oct')
+    material_calculator.formation_energy_interstitial('W', 'tet')
         
 if __name__ == "__main__":
     main()
-
